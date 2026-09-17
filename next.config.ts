42a41988cd68da4bafeb846e735cd089bb13bcb3
Billal Fauzan <billal.xcode@gmail.com>
2026-09-17 07:32:40 +0700

    feat: update multiformats dependency to version 14.0.5 and add additional remote image patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "assets.coingecko.com" },
       { protocol: "https", hostname: "coin-images.coingecko.com" },
       { protocol: "https", hostname: "robinscan.io" },
+      { protocol: "https", hostname: "dweb.link" },
+      { protocol: "https", hostname: "ipfs.io" },
+      { protocol: "https", hostname: "gateway.pinata.cloud" },
+      { protocol: "https", hostname: "*.mypinata.cloud" },
     ],
   },
 };
